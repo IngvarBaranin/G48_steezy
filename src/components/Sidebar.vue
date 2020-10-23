82b6e059a327cb013c0ab8a50a3bbe6453944b4b
Ingvar <ingvar@tmd.ee>
2020-10-23 17:37:57 +0300

    sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+
+    <v-main>
+        <v-navigation-drawer>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title text-h3">
+                        Steezy
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        Cyber Security hygiene
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+        </v-navigation-drawer>
+    </v-main>
+
+  </v-app>
+</template>
+
+<style>
+</style>
