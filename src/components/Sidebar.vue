451a6d29a4a4df58950966e0998f8fd059d28e14
Ingvar <ingvar@tmd.ee>
2020-10-23 18:00:13 +0300

    generic fields for sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
   <v-app>
 
     <v-main>
-        <v-navigation-drawer>
+        <v-navigation-drawer permanent>
             <v-list-item>
                 <v-list-item-content>
                     <v-list-item-title class="title text-h3">
@@ -13,6 +13,22 @@
                     </v-list-item-subtitle>
                 </v-list-item-content>
             </v-list-item>
+
+            <v-divider></v-divider>
+            <v-list
+              dense
+              nav
+            >
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title class = "text-h6 my-3 mx-4"><a href="overview">Overview</a></v-list-item-title>
+                  <v-list-item-title class = "text-h6 my-3 mx-4"><a href="advice">Advice</a></v-list-item-title>
+                  <v-list-item-title class = "text-h6 my-3 mx-4"><a href="resources">Resources</a></v-list-item-title>
+                  <v-list-item-title class = "text-h6 my-3 mx-4"><a href="contact">Contact</a></v-list-item-title>
+                  <v-list-item-title class = "text-h6 my-3 mx-4"><a href="news">News</a></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </v-main>
 
@@ -20,4 +36,5 @@
 </template>
 
 <style>
+
 </style>
