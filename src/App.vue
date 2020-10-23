a45ab7870bcbfaeb68c5f980f0b81382f6c9488b
rebekkabb <breedis.rebekka@gmail.com>
2020-10-23 20:10:51 +0300

    Created Contentbox and added it to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
 <template>
-  <v-app>
-              <Navbar/>
-              <Sidebar/>
-  </v-app>
+    <v-app>
+        <v-row>
+            <v-col cols="6"
+                   md="3">
+                <Sidebar/>
+            </v-col>
+
+            <v-col cols="12"
+                   sm="6"
+                   md="8">
+                <Navbar/>
+                <Contentbox/>
+            </v-col>
+        </v-row>
+
+
+    </v-app>
 </template>
 
 <script>
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Topbar';
+    import Sidebar from './components/Sidebar';
+    import Navbar from './components/Topbar';
+    import Contentbox from './components/Contentbox';
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  components: {
-    Sidebar,
-      Navbar
-  },
+        components: {
+            Sidebar,
+            Navbar,
+            Contentbox
+        },
 
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+            //
+        }),
+    };
 </script>
 
 <style>
-body {
-    width: 1200px;
-    margin:0 auto;
-}
+    body {
+        width: 1200px;
+        margin: 0 auto;
+    }
 </style>
